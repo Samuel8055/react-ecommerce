5c361e56b7eedb39b491ec693686282a231f1368
Samuel8055 <samueltschweitzer@gmail.com>
2021-12-31 11:36:05 +0530

    Consume products from api, implemented loading and error message component

---
+++ frontend/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Loading } from "./Loading";
+export { default as Message } from "./Message";
+export { default as Product } from "./Product";
+export { default as Rating } from "./Rating";
