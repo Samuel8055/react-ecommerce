3ebf24e4b15ac13f41b1bb45a73c670080ba0bb6
Samuel8055 <samueltschweitzer@gmail.com>
2022-01-11 15:17:38 +0530

    Implemented redux for Add to cart

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
+import { cartReducer } from "./cartReducers";
 import { productDetailsReducer, productListReducers } from "./productReducers";
 
 export const allReducers = combineReducers({
   productList: productListReducers,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
