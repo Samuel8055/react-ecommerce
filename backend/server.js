2480f159a297b4fa4f0986c228df9cee4151fa41
Samuel8055 <samueltschweitzer@gmail.com>
2022-01-12 00:02:59 +0530

    Created models and routes for product list and product details

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-import { data } from "./data.js";
+import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
 
 const app = express();
@@ -11,26 +11,8 @@ mongoose.connect(
 
 const port = process.env.port || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Server is ready!");
-});
-
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find(
-    (product) => product._id === req.params.id
-  );
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product not found!" });
-  }
-});
-
 app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
